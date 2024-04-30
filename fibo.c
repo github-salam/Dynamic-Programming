f1793c43a2f1ff308d5471fdedcaaea2fd0bea51
abdus-salam <abdussalam0419@gmail.com>
2024-04-30 19:49:13 +0600

    tabular method (dp-bottomUp) with counter varible for checking

--- fibo.c
+++ fibo.c
@@ -1,21 +1,23 @@
-//find nth term of a fibonacci number
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int count = 0;
 
-
-int fib(int n, int* memo){
-    if(memo[n] != -1){
-        return memo[n];
+int fib(int n, int* t){
+    if(n == -1){
+        return -1;
     }
-    count++;
-    if(n<0) return -1;
-    if(n == 0 || n == 1) return n;
-    int sum = fib(n-1, memo) + fib(n-2, memo);
-    memo[n] = sum;
-    return sum;
+    else if(n == 0) t[0] = 0;
+    else if(n == 1) t[1] = 1;
+    else{
+        t[0] = 0;
+        t[1] = 1;
+        for(int i = 2; i <= n; i++){
+            t[i] = t[i - 1] + t[i - 2];
+            count++;
+        }
+    }
+    return t[n];
 }
 
 
@@ -24,16 +26,11 @@ int main(){
     printf("Enter the nth number : ");
     int n;
     scanf("%d", &n);
-    int* memo = (int*)malloc((n+1)*sizeof(int));
-    if(memo == NULL){
-        printf("Memory allocation failed!");
-        return 1;
-    }
-    for(int i = 0; i <= n; i++){
-        memo[i] = -1;
-    }
-    int result = fib(n, memo);
+    int* T = (int *)malloc((n+1) * sizeof(int));
+    int result = fib(n,T);
     printf("%d\n", count);
-    free(memo);
+
+    free(T);
+
     return 0;
 }
